091bc13513579511157f615506c68109f32d40a5
Devendra Mandale <mandaledevendra2@gmail.com>
2026-07-12 20:47:47 +0530

    Remove database password from code

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="parking@123",
-        database="smart_parking"
+        host="smartparking-mysql-36d13ba7-smartparkingdev.l.aivencloud.com",
+        user="avnadmin",
+        password=os.environ.get("DB_PASSWORD"),
+        database="defaultdb",
+        port=17094,
+        ssl_disabled=False
     )
     return connection
